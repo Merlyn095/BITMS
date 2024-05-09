666719dfa5fcd3d8b3ea57e33af700f714702c5d
Merlyn095 <167085698+Merlyn095@users.noreply.github.com>
2024-05-09 11:39:27 +0530

    Add files via upload

---
+++ bike.py
@@ -0,0 +1,12 @@
+class re:
+    def __init__(self,name,color,mileage):
+        self.n=name
+        self.cl=color
+        self.m=mileage
+a=re("classic 350","black","10")
+b=re("xyz","red","10")
+c=re("xyz","red","10") 
+print(a.n,a.cl,a.m)
+print(b.n,b.cl,b.m)
+print(c.n,c.cl,c.m)   
+
